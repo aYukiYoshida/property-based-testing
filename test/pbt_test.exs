9b94c7c7612b0b2ba74a49c5a172a4513ab3423b
aYukiYoshida <yuki.yoshida@access-company.com>
2024-02-18 23:05:20 +0900

    プロパティの構造の整理

--- test/pbt_test.exs
+++ test/pbt_test.exs
@@ -2,13 +2,20 @@ defmodule PbtTest do
   use ExUnit.Case
   use PropCheck
 
-  property "always works" do
-    forall type <- term() do
+  # プロパティ
+  property "プロパティが何をするかの説明" do
+    forall type <- my_type() do
       boolean(type)
     end
   end
 
+  # ヘルパー
   def boolean(_) do
     true
+  end
+
+  # ジェネレータ
+  def my_type() do
+    term()
   end
 end
